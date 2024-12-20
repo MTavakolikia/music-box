741c1e55f5a54d2827ee732ec22ad72c2713dc93
mohammadtk1994 <mohammadtavakolikia66@gmail.com>
2024-12-20 20:31:59 +0330

    initial music box app project

---
+++ next.config.ts
@@ -0,0 +1,20 @@
+import { NextConfig } from 'next'
+
+const config: NextConfig = {
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+        port: '',
+        pathname: '/my-bucket/**',
+        search: '',
+      },
+    ],
+  },
+}
+
+export default config
